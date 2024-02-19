d4695648061e3022c8da8d07a127c4d86e9c352f
Dane Jacobson <jacobson.dane@gmail.com>
2024-02-19 17:48:15 -0500

    VGGNet-A implementation

--- vggnet/vggnet.py
+++ vggnet/vggnet.py
@@ -1,2 +1,69 @@
 """Implementation of VGGNet"""
 
+import torch.nn as nn
+
+class VGGNet_A(nn.Module):
+
+    def __init__(self):
+        super(VGGNet_A, self).__init__()
+
+        self.c1 = nn.Sequential([
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ])
+        self.c2 = nn.Sequential([
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ])
+        self.c3 = nn.Sequential([
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ])
+        self.c4 = nn.Sequential([
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ])
+        self.c5 = nn.Sequential([
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ])
+
+        self.fc1 = nn.Sequential([
+            nn.Linear(in_features=512 * 7 * 7, out_features=4096),
+            nn.ReLU(),
+        ])
+        self.fc2 = nn.Sequential([
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+        ])
+        self.fc3 = nn.Sequential([
+            nn.Linear(in_features=4096, out_features=1000),
+            nn.ReLU(),
+        ])
+
+        self.sm = nn.Softmax(dim=1000)
+
+        self.net = nn.Sequential([
+            self.c1,
+            self.c2,
+            self.c3,
+            self.c4,
+            self.c5,
+            self.fc1,
+            self.fc2,
+            self.fc3,
+            self.sm,
+        ])
+
+    def forward(self, x):
+        return self.net(x)
+    
+    
